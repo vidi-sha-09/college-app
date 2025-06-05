49bf346eb41ad1937cf815b0c570bb2bf49d919e
Vidisha <vidisha09pandey@gmail.com>
2025-06-05 13:49:24 +0530

    first commit

---
+++ src/main/java/com/app/college/collegeApp/CollegeAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.college.collegeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/college/collegeApp/CollegeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.college.collegeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
